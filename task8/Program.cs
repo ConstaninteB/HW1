31f728d7598321199840531dad680ee290808eb9
Constantine <rooki12@mail.ru>
2022-11-12 18:48:08 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите начало диапозона:");  
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите до какого числа диапозон:");  
+int number2 = Convert.ToInt32(Console.ReadLine());
+int count = number1;
+
+while(count < number2)
+{
+    count++;
+    
+      if(count % 2 == 0)
+      {
+          Console.WriteLine(count);
+      }
+}
